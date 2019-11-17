52eb8fc69c7e283df3ba1874da4c68a36ba481b3
KKneasel <55756453+KKneasel@users.noreply.github.com>
2019-11-17 15:25:13 -0500

    CategoryList added
    CategoryList added

--- Sprint1intoSprint2/src/sprint1/CategoryList.java
+++ Sprint1intoSprint2/src/sprint1/CategoryList.java
@@ -1,7 +1,25 @@
 
 package sprint1;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 
 public class CategoryList {
+    ObservableList<Category> list = FXCollections.observableArrayList();
+
+    public CategoryList() {
+        initializeList();
+    }
+
+    public ObservableList<Category> getList() {
+        return list;
+    }
+
+    private void initializeList() {
+        list.add(new Category("Fire Location", "Fire Fighters"));
+        list.add(new Category("Human Injuries","EMS"));
+        list.add(new Category("Road Obstruction"));
+    }
     
 }
